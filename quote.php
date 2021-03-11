1d9285180b67cc6023ad260e00a545a9c691c7ff
giuseppemoffa <80477440+giuseppemoffa@users.noreply.github.com>
2021-03-11 21:09:59 +0000

    Add files via upload

---
+++ quote.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content="Want to maximise efficiency in excel? We Automate Processes to Save You Time, So You can Focus on What Matters by Creating a Bespoke Solution.">
+     <link rel="icon" type="image/x-icon" href="consulting logo square.png" />
+      <title>Maximise Efficiency with Custom Automated Solutions, built by professionals at Excel and Master</title>
+
+       <!-- CSS  -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Open+Sans:wght@400;600&family=Oswald&family=Poppins:wght@500&family=Source+Serif+Pro:wght@600&family=Yantramanav:wght@500;700&display=swap"          rel="stylesheet">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+              <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+                <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+                 <!--Let browser know website is optimized for mobile-->
+                   <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86">
+				   
+				   <!-- Global site tag (gtag.js) - Google Ads: 659750461 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-659750461"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-659750461');
+  gtag('config', 'UA-190668359-1');
+</script>
+
+                 </head>
+                 <body class="contact-us"><nav class="navbar-default white" role="navigation">
+                     <div class="nav-wrapper">
+                        <img src="logo.png" class="img-fluid" alt="Excel and Master Logo">
+                            <ul class="right hide-on-med-and-down">
+                                 <li><a href="index.html">HOME</a></li>
+                                 <li><a href="about.html">ABOUT US</a></li>
+                                 <li><a href="quote.php">GET A QUOTE</a></li>
+                                     </ul>
+
+                   <ul id="nav-mobile" class="sidenav">
+                                <li><a href="index.html">HOME</a></li>
+                                <li><a href="about.html">ABOUT US</a></li>
+                                <li><a href="quote.php">GET A QUOTE</a></li>
+                               </ul>
+
+                 <a href="#" data-target="nav-mobile" class="sidenav-trigger">
+                   <i class="material-icons">menu</i></a>
+                  </div>
+                </nav>
+                <div class="container1">
+                    <form role="form" action="mail.php" method="post" class="form" id="contact">
+                            <h2>Get In Touch</h2>
+                           <h4>Interested in learning more about our services? Contact us for a custom quote!</h4>
+
+                              <select name="requirements" class="dropdown" required>
+                                  <option value="New spreadsheet">Please select requirements</option>
+                                  <option value="New spreadsheet">New spreadsheet</option>
+                                  <option value="extra functionality in existing spreadsheet">Extra functionality in existing spreadsheet</option>
+                                  <option value="fix existing spreadsheet">Fix existing spreadsheet</option>
+                                  <option value="other">Other</option>
+                             </select>
+
+                     <textarea id="textarea" name="message" placeholder="Describe your requirements as much as possible...." tabindex="3" required></textarea>
+
+                             <select id="system" name="system" class="dropdown" list="system">
+                                  <option value="selected">Please select operating system</option>
+                                  <option value="Excel on Windows">Excel on Windows</option> 
+                                  <option value="Excel on Mac">Excel on Mac</option>
+                                  <option value="Excel on Tablet/Mobile">Excel on Tablet/Mobile</option>
+                            </select>
+
+                            <input placeholder="Full Name" name="name" type="text" tabindex="2" required autofocus/>
+
+                            <input placeholder="Email Address" name="email" type="email" tabindex="3" required/>
+
+                            <input placeholder="Phone Number (optional)" name="number" type="tel" tabindex="4"/>
+
+                            <input placeholder="Company Name (optional)" name="company" type="text" tabindex="5"/>
+
+                            <br>
+
+                            <button form="contact" type="submit" id="contact-submit">Submit</button>
+
+                           </form>
+
+                         </div>
+
+                         <div id="success_message" style="width:100%; height:100%; display:none; ">
+                              <h2></h2>
+                            </div>
+                            <footer id="footer">
+                               <ul class="list center">
+                                 <li>            © Excel & Master. All rights reserved.        </li>
+                                 <li>            info@excelandmaster.com        </li>
+                                 <li>            <a href="">Terms &amp; Conditions</a>        </li>
+                                 <li>            <a href="">Privacy Policy</a>        </li>    </ul>
+                                 <ul class="list center">        Excel & Master | London | UK    </ul>
+                               </footer>
+
+                               <!--  Scripts-->
+
+                               <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+                               <script src="js/materialize.js"></script><script src="js/init.js"></script>
+
+                               <!--JavaScript at end of body for optimized loading-->
+
+                               <script type="text/javascript" src="js/materialize.min.js"></script>
+
+                             </body>
+                             </html>
